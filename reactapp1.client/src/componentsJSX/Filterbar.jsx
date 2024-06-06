6373e93dcc65924d0096bf4cbb0288d8ca92e57a
陳茜 <aass600717@gmail.com>
2024-06-07 03:00:26 +0800

    改選單的圖

--- reactapp1.client/src/componentsJSX/Filterbar.jsx
+++ reactapp1.client/src/componentsJSX/Filterbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Filterbar.scss';
 
-const FilterItem = ({ title, children }) => {
+const FilterItem = ({ title, children, isGrid }) => {
     return (
         <div className="filter-item">
             <button className="filter-button">{title}</button>
             <div className="dropdown">
-                <div className="dropdown-content">
+                <div className={`dropdown-content ${isGrid ? 'grid' : 'list'}`}>
                     {children}
                 </div>
             </div>
@@ -14,21 +14,24 @@ const FilterItem = ({ title, children }) => {
     );
 };
 
-const DropdownItem = ({ imgSrc, label }) => {
+const GridDropdownMenu = ({ items }) => {
     return (
-        <div className="location">
-            <img src={imgSrc} alt={label} />
-            <a href="#">{label}</a>
-        </div>
+        <ul className="dropdown-menu grid">
+            {items.map((item, index) => (
+                <li key={index} className="location">
+                    {item.imgSrc && <img src={item.imgSrc} alt={item.label} />}
+                    <a href="#">{item.label}</a>
+                </li>
+            ))}
+        </ul>
     );
 };
 
-const DropdownMenu = ({ items }) => {
+const ListDropdownMenu = ({ items }) => {
     return (
-        <ul className="dropdown-menu">
+        <ul className="dropdown-menu list">
             {items.map((item, index) => (
                 <li key={index}>
-                    {item.imgSrc && <img src={item.imgSrc} alt={item.label} style={{ width: '100px', height: '100px', marginRight: '10px' }} />}
                     {item.label}
                 </li>
             ))}
@@ -39,8 +42,8 @@ const DropdownMenu = ({ items }) => {
 const FilterBar = () => {
     return (
         <div className="filter-bar">
-            <FilterItem title="探索目的地">
-                <DropdownMenu items={[
+            <FilterItem title="探索目的地" isGrid={true}>
+                <GridDropdownMenu items={[
                     { imgSrc: "/src/assets/images/areas/taipei.jpg", label: "台北" },
                     { imgSrc: "/src/assets/images/areas/taichung.jpg", label: "台中" },
                     { imgSrc: "/src/assets/images/areas/hualien.jpg", label: "花蓮" },
@@ -50,50 +53,50 @@ const FilterBar = () => {
                     { imgSrc: "/src/assets/images/areas/yuling.jpg", label: "雲林" },
                 ]} />
             </FilterItem>
-            <FilterItem title="全部分類">
-                <DropdownMenu items={[
+            <FilterItem title="全部分類" isGrid={false}>
+                <ListDropdownMenu items={[
                     { label: "分類1" },
                     { label: "分類2" },
                     { label: "分類3" }
                 ]} />
             </FilterItem>
-            <FilterItem title="行程&體驗">
-                <DropdownMenu items={[
+            <FilterItem title="行程&體驗" isGrid={false}>
+                <ListDropdownMenu items={[
                     { label: "體驗1" },
                     { label: "體驗2" },
                     { label: "體驗3" }
                 ]} />
             </FilterItem>
-            <FilterItem title="景點門票">
-                <DropdownMenu items={[
+            <FilterItem title="景點門票" isGrid={false}>
+                <ListDropdownMenu items={[
                     { label: "門票1" },
                     { label: "門票2" },
                     { label: "門票3" }
                 ]} />
             </FilterItem>
-            <FilterItem title="飯店">
-                <DropdownMenu items={[
+            <FilterItem title="飯店" isGrid={false}>
+                <ListDropdownMenu items={[
                     { label: "飯店1" },
                     { label: "飯店2" },
                     { label: "飯店3" }
                 ]} />
             </FilterItem>
-            <FilterItem title="交通">
-                <DropdownMenu items={[
+            <FilterItem title="交通" isGrid={false}>
+                <ListDropdownMenu items={[
                     { label: "交通1" },
                     { label: "交通2" },
                     { label: "交通3" }
                 ]} />
             </FilterItem>
-            <FilterItem title="美食品嚐">
-                <DropdownMenu items={[
+            <FilterItem title="美食品嚐" isGrid={false}>
+                <ListDropdownMenu items={[
                     { label: "美食1" },
                     { label: "美食2" },
                     { label: "美食3" }
                 ]} />
             </FilterItem>
-            <FilterItem title="台灣高鐵">
-                <DropdownMenu items={[
+            <FilterItem title="台灣高鐵" isGrid={false}>
+                <ListDropdownMenu items={[
                     { label: "高鐵1" },
                     { label: "高鐵2" },
                     { label: "高鐵3" }
