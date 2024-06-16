f8fb9c58412030a2a7f29725cd73af0c7dc4b267
FUCK <al4yyds@gmail.com>
2024-06-16 17:30:40 +0800

    reset searchPage to empty

--- src/componentsJSX/SearchResults.jsx
+++ src/componentsJSX/SearchResults.jsx
@@ -1,41 +1,15 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import "./SearchResults.css";
+import "./SearchResults.scss";
 
 const SearchResults = ({ results }) => {
-  const navigate = useNavigate();
-
-  const handleClick = (id) => {
-    navigate(`/productpage/${id}`);
-  };
-
-  const formatDate = (dateString) => {
-    const options = { year: "numeric", month: "2-digit", day: "2-digit" };
-    return new Date(dateString).toLocaleDateString(undefined, options);
-  };
-
-  const truncateDescription = (description) => {
-    return description.length > 15
-      ? description.substring(0, 15) + "..."
-      : description;
-  };
-  console.log(results);
   return (
     <div className="search-results">
       {results.map((result) => (
-        <div
-          key={result.activityId}
-          className="search-result-item"
-          onClick={() => handleClick(result.activityId)}
-        >
-          <img
-            src={`data:image/png;base64,${result.photo[0]}`}
-            alt={result.name}
-          />
+        <div key={result.id} className="search-result-item">
+          <img src={result.image} alt={result.title} />
           <div className="result-details">
-            <h3>{result.name}</h3>
-            <p>{formatDate(result.date)}</p>
-            <p>{truncateDescription(result.description)}</p>
+            <h3>{result.title}</h3>
+            <p>{result.description}</p>
             <div className="result-meta">
               <span className="price">NT$ {result.price}</span>
               <span className="rating">{result.rating}</span>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,103 +1,166 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import FilterSidebar from '../componentsJSX/FilterSidebar';
-import SearchResults from '../componentsJSX/SearchResults';
-import Pagination from '../componentsJSX/Pagination';
-import './Search.scss';
-import backgroundImage from '../assets/images/contact/sectionBG.jpg';
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import FilterSidebar from "../componentsJSX/FilterSidebar";
+import SearchResults from "../componentsJSX/SearchResults";
+import "./Search.scss";
+import backgroundImage from "../assets/images/contact/sectionBG.jpg";
 
 const Search = () => {
   const [filters, setFilters] = useState({});
-  const [sortOrder, setSortOrder] = useState('Lookday 推薦');
+  const [sortOrder, setSortOrder] = useState("Lookday 推薦");
   const [results, setResults] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [resultsPerPage] = useState(9);
   const location = useLocation();
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true); // New state for loading
 
-  const queryParams = new URLSearchParams(location.search);
-  const queries = queryParams.getAll('query');
+  const query = new URLSearchParams(location.search).get("query");
 
   const sortResults = (results, order) => {
     switch (order) {
-      case '價格低到高':
+      case "價格低到高":
         return results.sort((a, b) => a.price - b.price);
-      case '價格高到低':
+      case "價格高到低":
         return results.sort((a, b) => b.price - a.price);
-      case '評分最高':
+      case "評分最高":
         return results.sort((a, b) => b.rating - a.rating);
       default:
         return results;
     }
   };
 
-  const fetchData = async () => {
-    try {
-      setLoading(true); // Start loading
-      const response = await fetch('https://localhost:7148/api/ActivityWithAlbum/');
-      if (!response.ok) {
-        throw new Error(`Http error! Status: ${response.status}`);
-      }
-      const data = await response.json();
+  useEffect(() => {
+    // 使用假數據
+    const data = [
+      {
+        id: 1,
+        image: "https://via.placeholder.com/300",
+        title: "景點 1",
+        description: "描述 1",
+        price: 100,
+        rating: "4.5",
+        location: "臺北",
+      },
+      {
+        id: 2,
+        image: "https://via.placeholder.com/300",
+        title: "景點 2",
+        description: "描述 2",
+        price: 200,
+        rating: "4.0",
+        location: "臺中",
+      },
+      {
+        id: 3,
+        image: "https://via.placeholder.com/300",
+        title: "景點 3",
+        description: "描述 3",
+        price: 300,
+        rating: "4.8",
+        location: "高雄",
+      },
+      {
+        id: 4,
+        image: "https://via.placeholder.com/300",
+        title: "景點 4",
+        description: "描述 4",
+        price: 400,
+        rating: "4.2",
+        location: "臺南",
+      },
+      {
+        id: 5,
+        image: "https://via.placeholder.com/300",
+        title: "景點 5",
+        description: "描述 5",
+        price: 500,
+        rating: "4.7",
+        location: "宜蘭",
+      },
+      {
+        id: 6,
+        image: "https://via.placeholder.com/300",
+        title: "景點 6",
+        description: "描述 6",
+        price: 600,
+        rating: "4.3",
+        location: "臺北",
+      },
+      {
+        id: 7,
+        image: "https://via.placeholder.com/300",
+        title: "景點 7",
+        description: "描述 7",
+        price: 700,
+        rating: "4.6",
+        location: "臺中",
+      },
+      {
+        id: 8,
+        image: "https://via.placeholder.com/300",
+        title: "景點 8",
+        description: "描述 8",
+        price: 800,
+        rating: "4.9",
+        location: "高雄",
+      },
+      {
+        id: 9,
+        image: "https://via.placeholder.com/300",
+        title: "景點 9",
+        description: "描述 9",
+        price: 900,
+        rating: "4.4",
+        location: "臺南",
+      },
+      {
+        id: 10,
+        image: "https://via.placeholder.com/300",
+        title: "景點 10",
+        description: "描述 10",
+        price: 1000,
+        rating: "4.1",
+        location: "宜蘭",
+      },
+    ];
 
-      const filteredResults = data.filter(item => {
-        const matchesQuery = queries.length > 0 ? queries.some(query => item.name.includes(query) || item.description.includes(query)) : true;
-        const matchesFilters = Object.keys(filters).every(filterKey => {
-          if (filterKey === 'priceRange') {
-            return item.price >= filters.priceRange.min && item.price <= filters.priceRange.max;
-          }
-          if (filterKey === 'dateRange') {
-            const itemDate = new Date(item.date);
-            const startDate = filters.dateRange.start ? new Date(filters.dateRange.start) : null;
-            const endDate = filters.dateRange.end ? new Date(filters.dateRange.end) : null;
-            return (!startDate || itemDate >= startDate) && (!endDate || itemDate <= endDate);
-          }
-          if (filterKey === 'locations') {
-            return filters.locations.includes(item.location);
-          }
-          return true;
-        });
-
-        return matchesQuery && matchesFilters;
+    // 根據搜索關鍵字和過濾條件過濾結果
+    const filteredResults = data.filter((item) => {
+      const matchesQuery = query
+        ? item.title.includes(query) || item.description.includes(query)
+        : true;
+      const matchesFilters = Object.keys(filters).every((filterKey) => {
+        return filters[filterKey].includes(item[filterKey]);
       });
+      return matchesQuery && matchesFilters;
+    });
 
-      const sortedResults = sortResults(filteredResults, sortOrder);
-      setResults(sortedResults);
-      setLoading(false); // Stop loading
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      setError(error.message);
-        setLoading(false); // Stop loading
-    }
-  };
+    const sortedResults = sortResults(filteredResults, sortOrder);
+    setResults(sortedResults);
+  }, [filters, sortOrder, query]);
 
   useEffect(() => {
-    fetchData();
-  }, [filters, sortOrder, queries]);
-
-  const indexOfLastResult = currentPage * resultsPerPage;
-  const indexOfFirstResult = indexOfLastResult - resultsPerPage;
-  const currentResults = results.slice(indexOfFirstResult, indexOfLastResult);
-
-  const paginate = pageNumber => setCurrentPage(pageNumber);
+    window.scrollTo(0, 0);
+  }, [location]);
 
   return (
     <div className="search-page">
-      <header className="search-header" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <header
+        className="search-header"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      >
         <div className="header-content">
-          <h1>{queries.length > 0 ? queries.join(', ') : '景點門票'}</h1>
+          <h1>{query ? query : "景點門票"}</h1>
           <p>探索主題樂園、博物館等眾多必遊景點</p>
         </div>
       </header>
       <div className="search-content">
         <FilterSidebar setFilters={setFilters} />
         <div className="search-results-wrapper">
-
           <div className="search-controls">
             <span>找到 {results.length} 項結果</span>
             <div className="search-sort">
-              <select value={sortOrder} onChange={e => setSortOrder(e.target.value)}>
+              <select
+                value={sortOrder}
+                onChange={(e) => setSortOrder(e.target.value)}
+              >
                 <option value="Lookday 推薦">Lookday 推薦</option>
                 <option value="價格低到高">價格低到高</option>
                 <option value="價格高到低">價格高到低</option>
@@ -106,20 +169,14 @@ const Search = () => {
             </div>
           </div>
           <div className="search-results-container">
-            {currentResults.length === 0 ? (
+            {results.length === 0 ? (
               <div className="no-results">
                 <p>搜索結果為 0，請重新搜索。</p>
               </div>
             ) : (
-              <SearchResults results={currentResults} />
+              <SearchResults results={results} />
             )}
           </div>
-          <Pagination
-            resultsPerPage={resultsPerPage}
-            totalResults={results.length}
-            paginate={paginate}
-            currentPage={currentPage}
-          />
         </div>
       </div>
     </div>
