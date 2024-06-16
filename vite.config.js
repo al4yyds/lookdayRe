f28228313b6712d6fb574e72e0b2d9b92e52687f
2024kang <166598158+2024kang@users.noreply.github.com>
2024-06-17 00:22:36 +0800

    第三方登入

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dns from "dns";
 
+dns.setDefaultResultOrder("verbatim");
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  //userlogin
+  server: {
+    host: "localhost",
+    port: 3000,
+  },
+});
